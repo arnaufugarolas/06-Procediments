870cf6763cbd1ccc815b654f0c54e1a0908fbd45
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-03-09 17:50:35 +0100

    Ex88 acabat

--- src/Ex88.java
+++ src/Ex88.java
@@ -26,7 +26,6 @@ public class Ex88 {
         int[] numbers = new int[0];
         int[] numbers2;
         int counter = 0;
-
         boolean stop = false;
         while (!stop) {
             int number = Main.LlegirEnter();
@@ -41,21 +40,8 @@ public class Ex88 {
                 stop = true;
             }
         }
-
         for (int number : numbers) {
             System.out.println(Main.Factorial(number));
         }
-
-
-
-
     }
 }
-
-/*
-88. Desenvolupar un programa per tal de demanar un conjunt de números pel teclat, finalitzant la
-sèrie amb el 0. Una vegada tots els números introduïts caldrà dir per a cada número quin és el seu
-factorial. Caldrà desenvolupar els següents mètodes
-• int LlegirEnter();
-• int factorial(int numero); ‘El factorial d’un nombre negatiu és 0
- */
